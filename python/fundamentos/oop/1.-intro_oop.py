9f72566800853cc2554c4209d3d52faf723af90e
juandonoso1661 <deiberdiaz@liceovvh.cl>
2026-04-27 10:38:46 -0400

    kpkjkl

---
+++ python/fundamentos/oop/1.-intro_oop.py
@@ -0,0 +1,13 @@
+# Esta es la sintaxis para crear una clase llamada Usuario:
+class Usuario:
+    def __init__(self):#Constructor
+        self.nombre = "Nariyoshi"
+        self.apellido = "Miyagi"
+        self.email = "miyagi@codingdojo.la"
+        self.limite_credito = 30000
+        self.saldo_pagar = 0
+pass #Pronto pondremos métodos y atributos
+
+# Y para crear una nueva instancia de nuestra clase se vería como:
+miyagi = Usuario()
+daniel = Usuario()
