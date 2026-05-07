7e8de5ba9d80c26189ce12d76f36d086853ee9ec
juandonoso1661 <deiberdiaz@liceovvh.cl>
2026-05-07 11:14:49 -0400

    jjaskdlas

---
+++ python/fundamentos/oop/atributos_metodos_clases.py
@@ -0,0 +1,91 @@
+# Atributos, métodos de clase, métodos estáticos
+
+#Definición de la clase
+class Estudiante: 
+    #Atributo de clase 
+    colegio = "Liceo Vate Vicente Huidobro"
+    #Lista en donde esten todos los estudiantes 
+    estudiantes = []
+    
+    #Metodo constructor 
+    def __init__(self, nombre, nota):
+        #Atributos de instrancias
+        self.nombre = nombre
+        self.nota = nota
+        
+        
+#Agregar elementos a lista Estudiante(objeto)
+        Estudiante.estudiantes.append(self)
+    
+    #Metodo de instancia
+    def mostrar_info(self):
+        print(f"Nombre : {self.nombre}")
+        print(f"Nota : {self.nota}")
+
+    #Metodo de clase 
+    #Usa "cls" porque trabaja con la información de la clase 
+    @classmethod
+    def cambiar_colegio(cls, nuevo_nombre):
+        cls.colegio = nuevo_nombre
+        
+    @classmethod # Contar la cantidad de estudiantes existentes
+    def cantidad_estudiantes(cls):
+        return len(cls.estudiantes)
+    
+    #Método estático
+    # Este no usar cls ni self, solo parametros
+    @staticmethod
+    def aprobar(nota):
+        if nota >= 4.0:
+            return True
+        else:
+            return False 
+    
+#Creación de objetos(instancias)
+e1 = Estudiante("camilo", 7.0)
+e2 = Estudiante("donovan", 6.4)
+e3 = Estudiante("matias", 3.2)
+
+#Uso de métodos de instancia
+print("==MÉTODO DE INSTANCIA==")
+e1.mostrar_info()
+print()
+e2.mostrar_info()
+print()
+
+#usar Atributo de clase
+print("===ATRIBUTO DE CLASE===")
+print(e1.colegio)
+print(e2.colegio)
+print()
+
+#Uso de metodo de clase
+print("===METODO DE CLASE===")
+Estudiante.cambiar_colegio("Purkuyen")
+print(e1.colegio)
+print(e2.colegio)
+
+#Contar Estudiante 
+print("===CONTAR ESTUDIANTES ===")
+print(f"Total estudiantes : {Estudiante.cantidad_estudiantes()}")
+
+#MÉTODO estático
+print("===MÉTODO ESTÁTICO===")
+
+## Función repaso.
+# Crear Función que valide usuario y contraseña
+
+def validador(user, password):
+    if user == "matias123" and password == "matias123":
+        print(f"Bienvenido, {user}")
+        return True
+    else:
+        print("Acceso denegado")
+        return False
+    
+def enviarDatos():
+    username = input( "Ingrese su nombre usuario : ")
+    password = input("Ingrese su contraseña : ")
+    validator = validador(username, password)
+
+enviarDatos()
--- python/prueba_grupos/prueba.py
+++ python/prueba_grupos/prueba.py
@@ -1,39 +1,74 @@
 import os
-
-def ingreso_numeros():
-    numeros = []
-    for i in range(5):
-        num = int(input("Ingresa un número: "))
-        numeros.append(num)
-    return numeros
-
-
 def positivos_negativos(numeros):
     for num in numeros:
         if num < 0:
             print("Ingresaste un número negativo:", num)
-            return false
+            return False
     
-    print(f"Ingresaste los siguientes numeros :{numeros}")
+    print(f"Ingresaste los siguientes numeros: {numeros}")
+    
+    
+def ingreso_numeros():
+    numeros = []
+    cantidad  = int(input("Ingresar cuantos numeros agregara : "))
+    for i in range(cantidad):
+        num = int(input("Ingresa un número :  "))
+        numeros.append(num)
+    return numeros
+
+# ----------- EJERCICIO EDADES ------------
+
+def clasificacionEdades(lista):
+    menores = 0
+    adultos = 0
+    mayores = 0
+
+    for i in range(len(lista)):
+        if lista[i] < 18:
+            menores += 1
+        elif lista[i] >= 18 and lista[i] < 60:
+            adultos += 1
+        elif lista[i] >= 60:
+            mayores += 1
+
+    print(f"Edades: {lista}")
+    print(f"Hay {menores} menores, {adultos} adultos y {mayores} adultos mayores.")
+
+
+def ingresoEdad():
+    cantidad = int(input("Ingresa cantidad de personas: "))
+    edades = []
+
+    for i in range(cantidad): 
+        edad = int(input("Ingrese la edad: "))
+        edades.append(edad)
+
+    clasificacionEdades(edades)
 
 
 def limpiar_consola():
     os.system('cls')
 
+
 # ----------- MENÚ ------------
 
+numeros = []
+
 while True:
-    print("\n1. Ingresar números")
-    print("2. Verificar")
+    print("\n1. ejercicio 1")
+    print("2. ejercicio 2")
     print("0. Salir")
 
     opcion = input("Opción: ")
 
     if opcion == "1":
         numeros = ingreso_numeros()
-
-    elif opcion == "2":
         positivos_negativos(numeros)
+    elif opcion == "2":
+        ingresoEdad()
 
     elif opcion == "0":
         break
+
+    else:
+        print("Opción inválida")
