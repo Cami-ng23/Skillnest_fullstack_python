97a0b36a03f7113f9b9d22418e996765a07e8de8
juandonoso1661 <deiberdiaz@liceovvh.cl>
2026-06-15 12:16:09 -0400

    sasdsad

--- python/core/bucles_for_básico_1_(Core)_camilo_14_04_2026/bucles_for_basico1.py
+++ python/core/bucles_for_básico_1_(Core)_camilo_14_04_2026/bucles_for_basico1.py
@@ -5,67 +5,70 @@ usando ejemplos inspirados en videojuegos y situaciones atractivas.
 
 # 1. Generador de niveles
 # Imprime todos los niveles del 0 al 100 (incluyendo el 100).
-# (Tu código aquí)
+
 def ejercicio1():
-    nivel = 0
-    while nivel <= 99:
-        nivel += 1
-    print(nivel)
+    for nivel in range(0, 101):
+        print(nivel)
+
 
 # 2. Potenciadores de energía (Múltiplos de 2)
 # Imprime los números múltiplos de 2 desde 2 hasta 500 (incluyendo el 500).
-# (Tu código aquí
+
 def ejercicio2():
-    for n in range (2 , 499 , 2):
+    for n in range(2, 501, 2):
         print(n)
+
 
 # 3. Trampa de emojis
 # Recorre los puntos del 1 al 100.
-# - Si el número es divisible por 5, imprime ""
-# - Si es divisible por 10, imprime ""
+# - Si el número es divisible por 5, imprime 🔥
+# - Si es divisible por 10, imprime 💀
 # ¡Cuidado con la prioridad en tus condicionales!
-# (Tu código aquí)
+
 def ejercicio3():
-    for i in range (1, 101):
-        if i  % 10 == 0 and i % 5 == 0:
-            print(f"{i} es divisible por 10 y 5")
+    for i in range(1, 101):
+        if i % 10 == 0:
+            print("💀")
         elif i % 5 == 0:
-            print(f"{i} es divisible por 5")
+            print("🔥")
+
 
 # 4. Suma colosal
 # Suma todos los números pares del 0 al 500,000 e imprime la suma total.
-# (Tu código aquí)
+
 def ejercicio4():
     sumatotal = 0
+
     for num in range(1, 500001):
         if num % 2 == 0:
             sumatotal += num
-    print(f"La suma total de los números pares de 500,000 es : {sumatotal}")
+
+    print(f"La suma total de los números pares de 500,000 es: {sumatotal}")
+
 
 # 5. Retroceso temporal
 # Desde 2024, retrocede de 3 en 3 hasta 0 o menos.
 # Imprime cada valor en la cuenta regresiva.
-# (Tu código aquí)
+
 def ejercicio5():
-    for temporal in range (2024 , 0 , -3):
+    for temporal in range(2024, -1, -3):
         print(temporal)
 
+
 # 6. Contador dinámico
-# Declara las variables inicio, fin, y salto (por ejemplo: inicio=3, fin=10, salto=2).
+# Declara las variables inicio, fin y salto.
 # Imprime los números en el rango que sean múltiplos de 'salto'.
-# (Tu código aquí)
+
 def ejercicio6():
     inicio = 3
     fin = 10
     salto = 2
+
     for multiplos in range(inicio, fin + 1):
         if multiplos % salto == 0:
-            print(multiplos)
-
-# Ejemplo: si inicio = 3, fin = 10, y salto = 2
-# Se imprimiría: 4, 6, 8, 10
-
+                print(multiplos)
 # ------------------ MENÚ ------------------
+
 while True:
     print("\n--- MENÚ DE EJERCICIOS ---")
     print("1.- Ejercicio 1")
